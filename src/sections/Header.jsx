663c75b9ca4f641cc2aec41c26f908bf3ed10c24
Hamza-Moh-amed <hamzamohamedworld@gmail.com>
2024-11-06 07:28:46 +0200

    second commit implementing features and pricing

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,20 +1,47 @@
 import clsx from "clsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
 
-const NavLink = ({ title }) => (
-  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
-    {title}
-  </LinkScroll>
-);
-
 const Header = () => {
+  const [hasScrolled, setHasScrolled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const NavLink = ({ title }) => (
+    <LinkScroll
+      onClick={() => setIsOpen(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass="nav-active"
+      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
+    >
+      {title}
+    </LinkScroll>
+  );
+
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10">
+    <header
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]"
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
-          <img src="/images/xora.svg" alt="logo" width={115} height={55} />
+          <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
 
         <div
@@ -31,10 +58,11 @@ const Header = () => {
                   <div className="dot" />
                   <NavLink title="pricing" />
                 </li>
+
                 <li className="nav-logo">
                   <LinkScroll
                     to="hero"
-                    offset={-100}
+                    offset={-250}
                     spy
                     smooth
                     className={clsx(
@@ -43,42 +71,43 @@ const Header = () => {
                   >
                     <img
                       src="/images/xora.svg"
-                      alt="logo"
                       width={160}
                       height={55}
+                      alt="logo"
                     />
                   </LinkScroll>
                 </li>
 
                 <li className="nav-li">
-                  <NavLink title="Faq" />
+                  <NavLink title="faq" />
                   <div className="dot" />
                   <NavLink title="download" />
                 </li>
               </ul>
             </nav>
 
-            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] translate-y-1/2 rotate-90">
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
               <img
                 src="/images/bg-outlines.svg"
-                alt="outline"
                 width={960}
                 height={380}
+                alt="outline"
                 className="relative z-2"
               />
               <img
                 src="/images/bg-outlines-fill.png"
-                alt="outline"
                 width={960}
                 height={380}
+                alt="outline"
                 className="absolute inset-0 mix-blend-soft-light opacity-5"
               />
             </div>
           </div>
         </div>
+
         <button
-          onClick={() => setIsOpen((prevState) => !prevState)}
           className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
+          onClick={() => setIsOpen((prevState) => !prevState)}
         >
           <img
             src={`/images/${isOpen ? "close" : "magic"}.svg`}
