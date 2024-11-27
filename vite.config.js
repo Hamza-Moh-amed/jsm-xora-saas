cd9200f6f60a896822cdb932e3f9b749f8434ca4
Hamza-Moh-amed <hamzamohamedworld@gmail.com>
2024-11-27 17:00:24 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-import Checker from "vite-plugin-checker";
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    [react()],
-    Checker({
-      typescript: false, // Disable TypeScript checker
-      eslint: {
-        lintCommand: "eslint .", // Disable or ignore lint errors
-      },
-    }),
-  ],
+  plugins: [react()],
 });
