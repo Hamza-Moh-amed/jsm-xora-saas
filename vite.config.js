7dbe971984881b2405d505085e04e36cf18766cc
Hamza-Moh-amed <hamzamohamedworld@gmail.com>
2024-11-27 16:38:43 +0200

    deploy error fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+import Checker from "vite-plugin-checker";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    [react()],
+    Checker({
+      typescript: false, // Disable TypeScript checker
+      eslint: {
+        lintCommand: "eslint .", // Disable or ignore lint errors
+      },
+    }),
+  ],
+});
