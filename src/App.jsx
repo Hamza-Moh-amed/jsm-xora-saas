9dc213e50152c523aae8354816b5bf3109d23d02
Hamza-Moh-amed <hamzamohamedworld@gmail.com>
2024-11-27 17:04:39 +0200

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
 import Faq from "./sections/Faq";
-import Testimonials from "./sections/testimonials";
 import Download from "./sections/Download";
 import Footer from "./sections/Footer";
+import Testimonials from "./sections/Testimonials";
 
 const App = () => {
   return (
